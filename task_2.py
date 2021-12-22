b0671ee2beafdfad61bbb21ffb24a1ca31a1554a
vladimir <vovazakharov2@gmail.com>
2021-12-22 12:03:05 +0300

    task_1 done

--- task_2.py
+++
@@ -1,7 +0,0 @@
-word_1 = b'class'
-word_2 = b'function'
-word_3 = b'method'
-
-print(f'{word_1} - type {type(word_1)}')
-print(f'{word_2} - type {type(word_2)}')
-print(f'{word_3} - type {type(word_3)}')
